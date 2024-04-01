db44359dd6d4964d56131a2304c84b61c38a9c58
anhbanlinhle <vietlinhleviet@gmail.com>
2024-04-02 01:27:27 +0700

    feat[lynn]: add express server config

---
+++ backend/src/server/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import session from 'express-session'
+import initWebRoute from '../route/web.js'
+
+import { config } from 'dotenv'
+config()
+
+const app = express()
+const port = process.env.PORT || 1111
+const cors = require("cors")
+
+app.use(cors({ 
+  credentials: true, 
+  origin: true
+}))
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(express.urlencoded({ 
+  extended: true 
+}))
+
+app.use(express.json())
+
+initWebRoute(app)
+
+app.listen(port, () => {
+  console.log(`\nApp running at:`)
+  console.log("\t\t\x1b[4m",`localhost:${port}`, "\x1b[0m")
+})
