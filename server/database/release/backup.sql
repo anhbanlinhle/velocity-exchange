88fa2b66de2332e8f9ca98238e1b06a0f4c3e541
anhbanlinhle <vietlinhleviet@gmail.com>
2024-05-04 15:58:00 +0700

    fix[lynn]: auto inc from 3 -> 1

--- server/database/release/backup.sql
+++ server/database/release/backup.sql
@@ -37,7 +37,7 @@ CREATE TABLE `account` (
   `phone_num` varchar(10) DEFAULT NULL,
   `is_admin` tinyint DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
