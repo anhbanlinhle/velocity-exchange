b9eaa88e374a6b766520a8a66e76818320d0cf17
anhensam4 <ahs2k3@gmail.com>
2024-05-06 17:08:45 +0700

    wip regis car

--- server/src/controller/admin/approvalRequest.js
+++ server/src/controller/admin/approvalRequest.js
@@ -1,42 +1,46 @@
-import pool from "../../config/database"
+import pool from "../../config/database";
 
 let approvalRequest = async (req, res) => {
-  let requestId = req.body.requestId
-  let adminId = req.body.adminId
-  let status = req.body.status
+  let requestId = req.body.requestId;
+  let adminId = req.body.adminId;
+  let status = req.body.status;
 
-  if (requestId === undefined || adminId === undefined || status === undefined) {
-    return res.status(422).send({ ErrorCode: "ER_MISSING_PARAM" })
+  if (
+    requestId === undefined ||
+    adminId === undefined ||
+    status === undefined
+  ) {
+    return res.status(422).send({ ErrorCode: "ER_MISSING_PARAM" });
   }
-  if (status !== 'APPROVED' && status !== 'REJECTED') {
-    return res.status(400).send({ ErrorCode: 'ER_INVALID_STATUS' })
+  if (status !== "APPROVED" && status !== "REJECTED") {
+    return res.status(400).send({ ErrorCode: "ER_INVALID_STATUS" });
   }
 
   try {
     let requestStatus = `
-    select status from verification_request where id = ?
-    `
-    let [rows1, dump1] = await pool.query(requestStatus, [requestId])
+      select status from verification_request where id = ?
+    `;
+    let [rows1, dump1] = await pool.query(requestStatus, [requestId]);
 
-    if (rows1[0].status !== 'PENDING') {
-      return res.status(400).send({ ErrorCode: 'ER_REQUEST_PROCESSED' })
+    if (rows1[0].status !== "PENDING") {
+      return res.status(400).send({ ErrorCode: "ER_REQUEST_PROCESSED" });
     }
 
     let query = `
-    update verification_request 
-    set status = ?, time = NOW(), adminId = ? 
-    where id = ?
-    `
+      update verification_request 
+      set status = ?, time = NOW(), adminId = ? 
+      where id = ?
+    `;
 
-    let [rows, fields] = await pool.query(query, [status, adminId, requestId])
+    let [rows, fields] = await pool.query(query, [status, adminId, requestId]);
 
-    return res.status(200).send({message: 'success'})
+    return res.status(200).send({ message: "success" });
   } catch (err) {
-    console.log(err)
-    return res.status(500).send({ ErrorCode: err.code, ErrorNo: err.errno })
+    console.log(err);
+    return res.status(500).send({ ErrorCode: err.code, ErrorNo: err.errno });
   }
-}
+};
 
 module.exports = {
-  approvalRequest
-}
+  approvalRequest,
+};
