322f0893afbf9507df5a744fea635111dc11ac85
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-05-06 15:11:21 +0700

    feat: add verification request page

---
+++ client/src/constant.js
@@ -0,0 +1,11 @@
+export const CardType = Object.freeze({
+  CAR: 'CAR',
+  AUCTION: 'AUCTION',
+  VERIFICATION_REQUEST: 'VERIFICATION_REQUEST',
+});
+
+export const Status = Object.freeze({
+  PENDING: 'PENDING',
+  APPROVED: 'APPROVED',
+  REJECTED: 'REJECTED',
+});
