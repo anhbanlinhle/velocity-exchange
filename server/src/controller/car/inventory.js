06dc0106fb501b1da8570b431e34e191636388e1
anhbanlinhle <vietlinhleviet@gmail.com>
2024-05-07 14:00:30 +0700

    fix[lynn]: return cars in inventory

--- server/src/controller/car/inventory.js
+++ server/src/controller/car/inventory.js
@@ -11,24 +11,19 @@ let inventoryRequest = async (req, res) => {
     let query = `
     select c.id as car_id, c.model_code, c.current_owner, c.class, c.status_in_storage, c.image,
     ac.first_name, ac.last_name,
-    max(v.id) as verification_request_id,
-    CASE
-        WHEN a.id IS NOT NULL THEN TRUE
-        ELSE FALSE
-    END AS is_auctioning
+    max(v.id) as verification_request_id
     from car as c
     join verification_request as v on c.id = v.car_id
     left join auction as a on a.car_id = c.id
     join account as ac on ac.id = v.seller_id
-    where v.seller_id = ? AND a.winner_id IS NULL
+    where v.seller_id = 1
     and (v.car_id, v.id) in
-    (select v.car_id, max(v.id)
-    from car as c
-    join verification_request as v on c.id = v.car_id
+      (select v.car_id, max(v.id)
+      from car as c
+      join verification_request as v on c.id = v.car_id
+      group by c.id
+      )
     group by c.id
-    )
-    group by c.id
-    ;
     `
 
     let [rows, fields] = await pool.query(query, [userId])
