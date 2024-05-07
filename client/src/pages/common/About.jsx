5ff3206d26657f5abb678ed246e18d3703c8a8b5
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-05-08 02:32:08 +0700

    feat: add lorem ispum

--- client/src/pages/common/About.jsx
+++ client/src/pages/common/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Typography } from '@mui/material';
+import PageTitle from '../../component/PageTitle';
 
 function About() {
   return (
-    <div>About</div>
+    <>
+      <PageTitle title="About" />
+      <Typography variant="body1" gutterBottom>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, rem nesciunt vel laborum aperiam, et quia sit ut atque est id blanditiis nisi eveniet! Nam deserunt quia praesentium libero nihil.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam architecto provident tenetur beatae quae, deserunt officiis unde vel modi praesentium quisquam corrupti assumenda incidunt, tempora nam. Assumenda obcaecati fuga omnis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit omnis adipisci iusto labore ut, dicta magnam rem nisi esse eius fuga cumque beatae nihil vitae officiis accusantium enim amet laborum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe facere totam odio aperiam, placeat tempora maiores quibusdam voluptatem iure sint ullam perspiciatis praesentium eaque, tenetur non rerum. Quisquam, omnis voluptatibus.
+      </Typography>
+    </>
   );
 }
 
