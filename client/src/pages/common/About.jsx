90a4aad6629b7f9c02156c4895cad8994b7ca20c
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-05-08 12:28:37 +0700

    Update About.jsx

--- client/src/pages/common/About.jsx
+++ client/src/pages/common/About.jsx
@@ -7,10 +7,10 @@ function About() {
     <>
       <PageTitle title="About" />
       <Typography variant="body1" gutterBottom>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, rem nesciunt vel laborum aperiam, et quia sit ut atque est id blanditiis nisi eveniet! Nam deserunt quia praesentium libero nihil.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam architecto provident tenetur beatae quae, deserunt officiis unde vel modi praesentium quisquam corrupti assumenda incidunt, tempora nam. Assumenda obcaecati fuga omnis.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit omnis adipisci iusto labore ut, dicta magnam rem nisi esse eius fuga cumque beatae nihil vitae officiis accusantium enim amet laborum?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe facere totam odio aperiam, placeat tempora maiores quibusdam voluptatem iure sint ullam perspiciatis praesentium eaque, tenetur non rerum. Quisquam, omnis voluptatibus.
+        Velocity Exchange (VEX) is a secure and transparent web-based auction platform designed specifically for the high-value segment of the automotive marketplace, focusing on supercars and hypercars. VEX eliminates the geographical constraints of traditional in-person auctions, allowing participants to engage in the bidding process from the comfort of their web-enabled devices (laptops, personal computers, tablets, and smartphones).
+        <br />
+        <br />
+        The project will involve developing a web-based platform accessible to end users through various internet-enabled devices such as laptops, desktops, tablets, and smartphones. Auction organizers will be able to register their car for listing listings onto the platform, providing details like product descriptions, starting prices, bid increments, auction start times, duration, and other relevant information. Users can browse through the listings, register to participate in auctions, and place bids by entering their desired bid amounts. Upon conclusion of an auction, the winning bidder will receive notification of their success and must complete payment within 24 hours. Failure to do so will result in the cancellation of the auction outcome, with arrangements made by the organizer as necessary.
       </Typography>
     </>
   );
